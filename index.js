ae4c699fac41bbc3a5df011c7ebe4b739bf4e356
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-29 09:45:18 +0530

    update port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1234;
+const port = process.env.PORT || 1234;
 
 app.get('/', (req, res) => {
 	res.json({ name: 'mayur', address: 'idar' });
