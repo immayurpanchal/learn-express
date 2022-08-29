f290384a5a85669310704afdd7016bc5cd3a690d
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-29 09:33:45 +0530

    basic server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 1234;
+
+app.get('/', (req, res) => {
+	res.json({ name: 'mayur', address: 'idar' });
+});
+
+app.get('/coin', (req, res) => {
+	res.json({ name: 'bitcoin', price: 123.456 });
+});
+
+app.get('/data', (req, res) => {
+	res.json([{ name: 'bitcoin' }, { name: 'dogecoin' }]);
+});
+
+// if you reach till here, means no route has matched
+app.get('*', (req, res) => {
+	res.status(169).json({ ans: 'route not found' });
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
